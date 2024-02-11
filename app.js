4122a510c7621aae43d32df344d211b7d2e38217
Ivan Kovalchuk <jivan0608@gmail.com>
2024-02-11 17:15:01 +0300

    serverbuild

--- app.js
+++ app.js
@@ -20,9 +20,13 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- models/blog.js
+++ models/blog.js
@@ -7,12 +7,5 @@ const blogSchema = new mongoose.Schema({
     likes: Number
   })
 
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
 
-module.exports = mongoose.model('blog', blogSchema)
+module.exports = mongoose.model('Blog', blogSchema)
